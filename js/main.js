9dcf17965809df18302891b04946abd944ceeb54
Mehedi-irfan <mehediirfan99@gmail.com>
2024-12-19 00:21:21 +0600

    footer section added

--- js/main.js
+++ js/main.js
@@ -46,3 +46,20 @@ toggleButton.addEventListener("change", () => {
   card_3_font.classList.toggle("-rotate-y-180");
   card_3_back.classList.toggle("rotate-y-180");
 });
+
+// navbar
+const navbar = document.querySelector("header");
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("bg-color-primary-dark");
+    navbar.classList.add("border-b");
+    navbar.classList.add("border-color-gray");
+    navbar.classList.add("duration-300");
+  } else {
+    navbar.classList.remove("bg-color-primary-dark");
+    navbar.classList.remove("border-b");
+    navbar.classList.remove("border-color-gray");
+    navbar.classList.remove("duration-300");
+  }
+};
